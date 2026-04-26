18e1bdf8242526997a5fa5ffa6c5dcf3f0000004
DarkCryptic102 <demolishedcryptic@gmail.com>
2026-04-26 10:12:33 +0530

    Update ClickGuiModule.java

--- src/main/java/me/alpha432/oyvey/features/modules/client/ClickGuiModule.java
+++ src/main/java/me/alpha432/oyvey/features/modules/client/ClickGuiModule.java
@@ -8,24 +8,28 @@ import me.alpha432.oyvey.features.gui.OyVeyGui;
 import me.alpha432.oyvey.features.modules.Module;
 import me.alpha432.oyvey.features.settings.Setting;
 import org.lwjgl.glfw.GLFW;
-
 import java.awt.*;
-
 import static me.alpha432.oyvey.features.commands.MessageSignatures.GENERAL;
 
 public class ClickGuiModule extends Module {
     private static ClickGuiModule INSTANCE;
 
     public final Setting<String> prefix = str("Prefix", ".");
-    public final Setting<Color> color = color("Color", 0, 0, 255, 180);
-    public final Setting<Color> topColor = color("TopColor", 0, 0, 150, 240);
+
+    // Duck theme: yellow/orange palette instead of blue
+    public final Setting<Color> color = color("Color", 255, 200, 0, 180);         // duck yellow
+    public final Setting<Color> topColor = color("TopColor", 230, 120, 0, 240);   // duck orange (beak/feet)
+
     public final Setting<Boolean> rainbow = bool("Rainbow", false);
     public final Setting<Integer> rainbowHue = num("Delay", 240, 0, 600);
     public final Setting<Float> rainbowBrightness = num("Brightness", 150.0f, 1.0f, 255.0f);
     public final Setting<Float> rainbowSaturation = num("Saturation", 150.0f, 1.0f, 255.0f);
 
+    // Duck Inc branding
+    private static final String DUCK_TITLE = "\uD83E\uDD86 Duck Inc";  // 🦆 Duck Inc
+
     public ClickGuiModule() {
-        super("ClickGui", "Opens the ClickGui", Module.Category.CLIENT);
+        super("ClickGui", "Opens the Duck Inc ClickGui", Module.Category.CLIENT);
         setBind(GLFW.GLFW_KEY_RIGHT_SHIFT);
         rainbowHue.setVisibility(v -> rainbow.getValue());
         rainbowBrightness.setVisibility(v -> rainbow.getValue());
@@ -38,7 +42,8 @@ public class ClickGuiModule extends Module {
         if (event.getType() == ClientEvent.Type.SETTING_UPDATE && event.getSetting().getFeature().equals(this)) {
             if (event.getSetting().equals(this.prefix)) {
                 OyVey.commandManager.setCommandPrefix(this.prefix.getPlannedValue());
-                Command.sendMessage("Prefix set to {global} %s", GENERAL, OyVey.commandManager.getCommandPrefix());
+                Command.sendMessage(DUCK_TITLE + " | Prefix set to {global} %s", GENERAL,
+                        OyVey.commandManager.getCommandPrefix());
             }
             if (event.getSetting().equals(this.color)) {
                 OyVey.colorManager.setColor(this.color.getPlannedValue());
@@ -69,5 +74,10 @@ public class ClickGuiModule extends Module {
 
     public static ClickGuiModule getInstance() {
         return INSTANCE;
+    }
+
+    /** Helper: returns the Duck Inc branding string for use in GUI renderers */
+    public static String getDuckTitle() {
+        return DUCK_TITLE;
     }
 }
